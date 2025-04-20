94a2148699fafe65263a90b1090f3c5667949edd
atharvakinage <kinageatharv@gmail.com>
2025-04-20 21:58:31 +0530

    Add shell script to automate the node start

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Terminal for node1
+gnome-terminal --title="Node 1" -- bash -c "
+echo 'Starting Node 1...';
+./raft3d --raft-id node1 --raft-port 12000 --http-port 8081 --raft-db raft1.db;
+exec bash"
+
+# Terminal for node2
+gnome-terminal --title="Node 2" -- bash -c "
+echo 'Starting Node 2...';
+./raft3d --raft-id node2 --raft-port 12001 --http-port 8082 --raft-db raft2.db --join 127.0.0.1:8081;
+exec bash"
+
+# Terminal for node3
+gnome-terminal --title="Node 3" -- bash -c "
+echo 'Starting Node 3...';
+./raft3d --raft-id node3 --raft-port 12002 --http-port 8083 --raft-db raft3.db --join 127.0.0.1:8081;
+exec bash"
+
